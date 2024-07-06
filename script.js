2befbd9fe6fc4fc9781e358d523fd7fb1e3eef58
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-07-06 21:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,4 +16,15 @@ function fibonacci(num) {
     return current;
 }
 
+// Example usage:
+console.log(fibonacci(0));  // Output: 0
+console.log(fibonacci(1));  // Output: 1
+console.log(fibonacci(2));  // Output: 1
+console.log(fibonacci(3));  // Output: 2
+console.log(fibonacci(4));  // Output: 3
+console.log(fibonacci(5));  // Output: 5
+console.log(fibonacci(6));  // Output: 8
+console.log(fibonacci(7));  // Output: 13
+
+
 
