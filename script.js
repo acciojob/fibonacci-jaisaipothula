b9eadfc9650f521e3a3d2607a67573899b8fe59f
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-09-27 21:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,21 +5,12 @@ function fibonacci(num) {
 
     let a = 0; // F(0)
     let b = 1; // F(1)
-    let fib;
 
     for (let i = 2; i < num; i++) {
-        fib = a + b; // F(n) = F(n-1) + F(n-2)
+        let fib = a + b; // F(n) = F(n-1) + F(n-2)
         a = b; // Move to the next Fibonacci number
         b = fib;
     }
 
-    return fib; // Return the nth Fibonacci number
+    return b; // Return the nth Fibonacci number
 }
-
-// Example test cases
-console.log(fibonacci(1)); // Output: 0
-console.log(fibonacci(2)); // Output: 1
-console.log(fibonacci(3)); // Output: 1
-console.log(fibonacci(4)); // Output: 2
-console.log(fibonacci(5)); // Output: 3
-console.log(fibonacci(10)); // Output: 34
